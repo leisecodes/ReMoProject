3a03d7329711b71d2197e158faae5968c9c23f61
Dooony <feng.xuey@northeastern.edu>
2023-11-22 10:07:16 -0500

    Add 3rd ScrollBar

--- src/pages/bookPage/ScrollBar/index.js
+++ src/pages/bookPage/ScrollBar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ScrollBar from './ScrollBar';
 import ScrollBar2 from './ScrollBar2';
+import ScrollBar3 from './ScrollBar3';
 import Message from './Message';
 import Message2 from './Message2';
 import Message3 from './Message3';
@@ -16,7 +17,9 @@ function ScrollBarMain() {
            
             <Message2 />
             <ScrollBar2 />
+
             <Message3 />
+            <ScrollBar3 />
         </div>
     )
 }
